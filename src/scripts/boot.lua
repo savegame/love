204978c3cea5c05976f9133c3aec13e31accf371
sashikknox <sashikknox@gmail.com>
2020-07-05 01:19:22 +0300

    - add gloabal love.sailfish in  boot.lua

--- src/scripts/boot.lua
+++ src/scripts/boot.lua
@@ -24,6 +24,7 @@ local love = require("love")
 -- Used for setup:
 love.path = {}
 love.arg = {}
+love.sailfish = {}
 
 -- Replace any \ with /.
 function love.path.normalslashes(p)
@@ -179,21 +180,23 @@ function love.arg.parseGameArguments(a)
 	return out
 end
 
-local begin_draw = function () end
-local end_draw = function () end
+-- TODO make global object love.sailfish with table of needed functions
+love.sailfish = {
+	begin_draw = function () end,
+ 	end_draw = function () end
+}
 local create_canvas = function () end -- when need new resolution for canvas
 local set_canvas = function () end -- when canvas is already exists
 local main_canvas = nil -- canvas handler
-local allowed_orientations = nil
-local allowed_orientation = 6 -- all
 local prev_orientation = "portrait"
 local convert_xy = function (x,y) return x,y;  end
 local lg_getWidth, lg_getHeight = nil, nil
 local lg_setCanvas = nil
--- local canvas_color = { r = 1, g = 1, b = 1, a = 1 }
+local lg_setColor = nil
 
 local lv_system = require("love.system")
 if lv_system.getOS() == "SailfishOS" then
+	print("SailfishOS detected")
 	-- set callbacks for functions
 	require("love.graphics")
 	require("love.window")
@@ -201,6 +204,7 @@ if lv_system.getOS() == "SailfishOS" then
 	lg_getWidth = love.graphics.getWidth
 	lg_getHeight = love.graphics.getHeight
 	lg_setCanvas = love.graphics.setCanvas
+	lg_setColor = love.graphics.setColor
 
 	love.graphics.setCanvas = function (canvas)
 		if canvas == nil then
@@ -226,23 +230,6 @@ if lv_system.getOS() == "SailfishOS" then
 			width = lg_getHeight()
 			height = lg_getWidth()
 		end
-		-- if allowed_orientation == allowed_orientations.all then
-		-- 	if o == "landscape" or o == "landscapeflipped" then
-		-- 		width = height
-		-- 		height = lg_getWidth()
-		-- 	end
-		-- 	-- current_orientation = o
-		-- elseif allowed_orientation == allowed_orientations.landscape then
-		-- 	if o == "landscape" or o == "landscapeflipped" then
-		-- 		current_orientation = o
-		-- 	end
-		-- 	width = height
-		-- 	height = lg_getWidth()
-		-- else
-		-- 	if o == "portrait" or o == "portraitflipped" then
-		-- 		current_orientation = o
-		-- 	end
-		-- end 
 
 		if current_orientation == "portrait" or current_orientation == "portraitflipped" then
 			convert_xy = function (x,y)
@@ -250,9 +237,9 @@ if lv_system.getOS() == "SailfishOS" then
 			end
 			love.graphics.getWidth = lg_getWidth
 			love.graphics.getHeight = lg_getHeight
-			end_draw = function ()
+			love.sailfish.end_draw = function ()
 				lg_setCanvas()
-				-- love.graphics.setColor(canvas_color)
+				lg_setColor(1,1,1)
 				-- love.graphics.clear()
 				love.graphics.draw(main_canvas, 0, 0, 0, 1, 1)
 				-- love.graphics.setColor(255,255,255,255)
@@ -267,7 +254,7 @@ if lv_system.getOS() == "SailfishOS" then
 		-- 	end
 		-- 	love.graphics.getWidth = lg_getWidth
 		-- 	love.graphics.getHeight = lg_getHeight
-		-- 	end_draw = function ()
+		-- 	love.sailfish.end_draw = function ()
 		-- 		love.graphics.setCanvas()
 		-- 		love.graphics.draw(main_canvas, lg_getWidth(), lg_getHeight(), math.pi, 1, 1)
 		-- 	end
@@ -277,9 +264,9 @@ if lv_system.getOS() == "SailfishOS" then
 			end
 			love.graphics.getWidth = lg_getHeight
 			love.graphics.getHeight = lg_getWidth
-			end_draw = function ()
+			love.sailfish.end_draw = function ()
 				lg_setCanvas()
-				-- love.graphics.setColor(canvas_color)
+				lg_setColor(1,1,1)
 				-- love.graphics.clear()
 				love.graphics.draw(main_canvas, lg_getWidth(), 0, math.pi * 0.5, 1, 1)
 				-- love.graphics.setColor(255,255,255,255)
@@ -294,9 +281,9 @@ if lv_system.getOS() == "SailfishOS" then
 			end
 			love.graphics.getWidth = lg_getHeight
 			love.graphics.getHeight = lg_getWidth
-			end_draw = function ()
+			love.sailfish.end_draw = function ()
 				lg_setCanvas()
-				-- love.graphics.setColor(canvas_color)
+				lg_setColor(1,1,1)
 				-- love.graphics.clear()
 				love.graphics.draw(main_canvas, 0, lg_getHeight(), math.pi * 1.5, 1, 1)
 				-- love.graphics.setColor(255,255,255,255)
@@ -309,25 +296,13 @@ if lv_system.getOS() == "SailfishOS" then
 		-- print(tostring(current_orientation) .. " is " .. width .. "x" ..height)
 		prev_orientation = current_orientation
 		lg_setCanvas(main_canvas)
-		begin_draw = set_canvas
+		love.sailfish.begin_draw = set_canvas
 	end
 
-	-- update_orientation = function (o)
-	-- 	if prev_orientation ~= o then 
-	-- 		if allowed_orientation == allowed_orientations.all then
-	-- 			begin_draw = create_canvas
-	-- 		end
-	-- 	end
-	-- end
-
-	-- end_draw = function ()
-	-- 	love.graphics.setCanvas()
-	-- end
-
 	if main_canvas == nil then
-		begin_draw = create_canvas
+		love.sailfish.begin_draw = create_canvas
 	else
-		begin_draw = set_canvas
+		love.sailfish.begin_draw = set_canvas
 	end
 end
 lv_system = nil
@@ -436,7 +411,7 @@ function love.createhandlers()
 			collectgarbage()
 		end,
 		displayrotated = function (display, orient)
-			begin_draw = create_canvas
+			love.sailfish.begin_draw = create_canvas
 			if love.displayrotated then return love.displayrotated(display, orient) end
 		end,
 	}, {
@@ -779,10 +754,10 @@ function love.run()
 		if love.graphics and love.graphics.isActive() then
 			love.graphics.origin()
 			-- for SailfishOS set global canvas
-			begin_draw()
+			love.sailfish.begin_draw()
 			love.graphics.clear(love.graphics.getBackgroundColor())
 			if love.draw then love.draw() end
-			end_draw()
+			love.sailfish.end_draw()
 			love.graphics.present()
 		end
 
@@ -879,10 +854,10 @@ function love.errhand(msg)
 		local width = 0
 		if (lg_setCanvas ~= nil) then width = lg_getWidth() else width = love.graphics.getWidth() end
 		if (lg_setCanvas ~= nil) then lg_setCanvas() else love.graphics.setCanvas() end
-		-- begin_draw()
+		-- love.sailfish.begin_draw()
 		love.graphics.clear(89/255, 157/255, 220/255)
 		love.graphics.printf(p, pos, pos, width - pos)
-		-- end_draw()
+		-- love.sailfish.end_draw()
 		love.graphics.present()
 	end
 
